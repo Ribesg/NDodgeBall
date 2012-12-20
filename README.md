ed241db347c422f72a8432be722f34e57c0295d8
Ribesg <Ribesg@Dragon>
2012-12-20 19:48:38 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+NDodgeBall
+==========
+The DodgeBall node of the N plugins suite
+Requires NCuboid node
---
+++ src/com/github/ribesg/ndodgeball/NDodgeBall.java
@@ -0,0 +1,7 @@
+package com.github.ribesg.ndodgeball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NDodgeBall extends JavaPlugin {
+
+}
