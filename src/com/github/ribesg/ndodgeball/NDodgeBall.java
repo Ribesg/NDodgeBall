8d4ba3d785bb69bb9bdb06a3b088bc73902d25eb
Ribesg <Ribesg@Dragon>
2012-12-20 20:10:19 +0100

    Link to Core Interfaces

--- src/com/github/ribesg/ndodgeball/NDodgeBall.java
+++ src/com/github/ribesg/ndodgeball/NDodgeBall.java
@@ -2,6 +2,27 @@ package com.github.ribesg.ndodgeball;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NDodgeBall extends JavaPlugin {
+import com.github.ribesg.ncore.NCore;
+import com.github.ribesg.ncore.nodes.dodgeball.DodgeBallNode;
+
+public class NDodgeBall extends JavaPlugin implements DodgeBallNode {
+
+	@Override
+	public void afterEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCore(NCore core) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public NCore getCore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
